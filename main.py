797452171a628a3c64481ead775ec75f5728bb9c
Aleksey Gusev <106704552+Algusb@users.noreply.github.com>
2023-02-26 17:32:11 +0000

    do bot

--- main.py
+++ main.py
@@ -1,56 +1,62 @@
-import random
+from random import choice
+import telebot
 
-HELP = """
-help - напечатать справку по программе.
-add - добавить задачу в список (название задачи запрашиваем у пользователя).
-show - напечатать все добавленные задачи.
-random - добавлять случайную задачу на дату Сегодня"""
+token = ''
+
+bot = telebot.TeleBot(token)
 
 RANDOM_TASKS = ["Улыбнись", "Не грусти", "Скажи добрые слова", "Займись самообразованием"]
 
-tasks = {
-
-}
-
-# Сегодня, Завтра, 31.12 ...
-# [Задача1, Задача2, Задача3]
-# Дата -> [Задача1, Задача2, Задача3]
+todos = dict()
 
 
-run = True
+HELP = '''
+Список доступных команд:
+* print  - напечать все задачи на заданную дату
+* todo - добавить задачу
+* random - добавить на сегодня случайную задачу
+* help - Напечатать help
+'''
+
 
 def add_todo(date, task):
-  if date in tasks:
-      # Дата есть в словаре
-      # Добавляем в список задачу
-      tasks[date].append(task)
-  else:
-      # Даты в словаре нет
-      # Создаем запись с ключом date
-      tasks[date] = []
-      tasks[date].append(task)
-  print("Задача ", task, " добавлена на дату ", date)
-
-while run:
-  command = input("Введите команду: ")
-  if command == "help":
-    print(HELP)
-  elif command == "show":
-    date = input("Введите дату для отображения списка задач: ")
-    if date in tasks:
-      for task in tasks[date]:
-        print('- ', task)
+    date = date.lower()
+    if todos.get(date) is not None:
+        todos[date].append(task)
     else:
-      print("Такой даты нет")
-  elif command == "add":
-    date = input("Введите дату для добавления задачи: ")
-    task = input("Введите название задачи: ")
-    add_todo(date, task)
-  elif command == "random":
-    task = random.choice(RANDOM_TASKS)
-    add_todo("Сегодня", task)
-  else: 
-    print("Неизвестная команда")
-    break
+        todos[date] = [task]
 
-print("До свидания!")
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, HELP)
+
+
+@bot.message_handler(commands=['random'])
+def random(message):
+    task = choice(RANDOM_TASKS)
+    add_todo('сегодня', task)
+    bot.send_message(message.chat.id, f'Задача {task} добавлена на сегодня')
+
+
+@bot.message_handler(commands=['add'])
+def add(message):
+    _, date, tail = message.text.split(maxsplit=2)
+    task = ' '.join([tail])
+    add_todo(date, task)
+    bot.send_message(message.chat.id, f'Задача {task} добавлена на дату {date}')
+
+
+@bot.message_handler(commands=['show'])
+def print_(message):
+    date = message.text.split()[1].lower()
+    if date in todos:
+        tasks = ''
+        for task in todos[date]:
+            tasks += f'[ ] {task}\n'
+    else:
+        tasks = 'Такой даты нет'
+    bot.send_message(message.chat.id, tasks)
+
+
+bot.polling(none_stop=True)
